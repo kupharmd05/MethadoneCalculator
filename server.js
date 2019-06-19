517093e6edfc0f35d2813cc089af05fed889e3c7
kupharmd05 <kupharmd05@gmail.com>
2019-06-18 21:23:57 -0500

    working on setting up server

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"));
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT,"0.0.0.0", function(){
     console.log("Listening on PORT: "+ PORT);
